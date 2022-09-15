4fe1a61561a59a98e384bb76d11baa7bb53d8017
JorgeED35 <jomarencarnacion3@gmail.com>
2022-09-15 17:03:17 -0500

    arrayObj

--- 03-templCreate.js
+++ 03-templCreate.js
@@ -4,23 +4,37 @@ const fragment = document.createDocumentFragment()
 const btnsButtons = document.querySelectorAll(".card .btn")
 
 
-const carritoObjeto = {}
+const carritoObjeto = []
 
 const addCarrito = (e) => {
-    console.log(e.target.dataset.fruta)
+  //  console.log(e.target.dataset.fruta)
 
     producto = {
         titulo: e.target.dataset.fruta,
         id: e.target.dataset.fruta,
         cantidad: 1,
     }
+    
+    const index = carritoObjeto.findIndex((item) => item.id === producto.id)
 
-    if(carritoObjeto.hasOwnProperty(producto.titulo)){
-        producto.cantidad = carritoObjeto[producto.id].cantidad + 1
-    } 
-    carritoObjeto[producto.id] = producto;
+    if (index === -1) {
+        carritoObjeto.push(producto)
+    }else{
+        carritoObjeto[index].cantidad ++
+    }
 
-    pintarCarrito(producto)
+   console.log(carritoObjeto)
+
+
+
+
+
+   // if(carritoObjeto.hasOwnProperty(producto.titulo)){
+   //     producto.cantidad = carritoObjeto[producto.id].cantidad + 1
+   // } 
+    //carritoObjeto[producto.id] = producto;
+
+    pintarCarrito(carritoObjeto)
 
 
 }
@@ -30,11 +44,11 @@ btnsButtons.forEach(btn =>{
 
 })
 
-const pintarCarrito = () =>{
+const pintarCarrito = (array) =>{
 
     carrito.textContent = ""
 
-    Object.values(carritoObjeto).forEach((item) => {
+    /* Object.values(carritoObjeto) */array.forEach((item) => {
         const clone = template.content.firstElementChild.cloneNode(true)
         clone.querySelector(".lead").textContent = item.titulo
         clone.querySelector(".badge").textContent = item.cantidad
